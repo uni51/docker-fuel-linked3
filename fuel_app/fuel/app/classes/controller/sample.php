1bd75c9fc50e1fb9a650488098d99a1aae893eb1
Koichi.Shimomura <unieye51@gmail.com>
2022-09-06 17:02:40 +0900

    4-1 ビューへの値の受け渡し

--- fuel_app/fuel/app/classes/controller/sample.php
+++ fuel_app/fuel/app/classes/controller/sample.php
@@ -2,12 +2,16 @@
 
 class Controller_Sample extends Controller {
 
+//    public function action_index() {
+//        $data = array();
+//        $data['name'] = 'たにぐち';
+//
+//        return View::forge('sample/index', $data);
+//    }
+
     public function action_index() {
-
-        $rows = DB::query('SELECT * FROM items')->execute();
-
-        foreach($rows as $row):
-            print($row['item_name'] . '/' . $row['price'] . "\n");
-        endforeach;
+        $view = View::forge('sample/index');
+        $view->set('name', 'たにぐち');
+        return $view;
     }
 }
